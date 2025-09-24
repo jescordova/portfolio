a8f57c658ec5075d6162f4f020ceb8509d5dd90d
Jesus Cordova <jescordova@gmail,com>
2025-09-24 04:36:18 +0200

    List of footer icons

---
+++ src/lib/js/icons.js
@@ -0,0 +1,14 @@
+export const social = [
+    {
+        href: 'https://portfolio-yasta.netlify.app/', 
+        label: 'yasta', 
+        name: 'bx bx-world', 
+        target: '_blanc'
+    },
+    {
+        href: 'https://github.com/jescordova', 
+        label: 'github', 
+        name: 'bx bxl-github', 
+        target: '_blanc'
+    },
+]
