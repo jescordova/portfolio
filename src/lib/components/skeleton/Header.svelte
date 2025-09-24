705f8d6f08a3aef780b1ce0b753b861d1c1f6c1a
Jesus Cordova <jescordova@gmail,com>
2025-09-24 12:42:10 +0200

    Created and added the Header component

---
+++ src/lib/components/skeleton/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Icons from "../Icons.svelte";
+    import { contacts } from '$lib/js/icons';
+
+    let { user } = $props();
+</script>
+
+<header>
+    <div class="user flex justify-between items-center flex-col md:flex-row">
+        <img 
+            src={user.src} 
+            alt="{user.first_name} {user.last_name}" 
+            class="w-25 rounded-md border-4 border-double border-(--info) shadow-lg mx-auto md:mx-0"
+        />
+        <div class="info text-center md:text-right">
+            <h1 class="text-3xl md:text-5xl">{user.first_name} <strong>{user.last_name}</strong></h1>
+            <h2 class="text-xl md:text-3xl">{user.diploma}</h2>
+            <div class="icons">
+                {#each contacts as icon}
+                    <Icons {...icon} />
+                {/each}
+            </div>
+        </div>
+    </div>
+    <div class="my-4">
+        <h3 class="text-xl">{user.presenting.title}</h3>
+        <p class="text-justify">{@html user.presenting.description}</p>
+    </div>
+</header>
---
+++ src/lib/js/infos.js
@@ -0,0 +1,11 @@
+export const user = {
+    src: 'petros.jpg',
+    first_name: 'Jesús',
+    last_name: 'Córdova',
+    diploma: 'Développeur Web',
+    presenting: {
+        title: 'Profil',
+        description: "<strong>Développeur web</strong> junior passionné par la création de sites et d'applications modernes. Motivé à apprendre et à progresser dans les technologies web <strong>front-end</strong> et <strong>back-end</strong>. Autodidacte, avec plusieurs projets réalisés dans un cadre personnel et de formation en ligne."
+    }
+    
+}
