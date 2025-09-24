398770100460a19e109b0000b84f42b94adf9697
Jesus Cordova <jescordova@gmail,com>
2025-09-24 04:13:22 +0200

    Created an Icon component

---
+++ src/lib/components/Icons.svelte
@@ -0,0 +1,13 @@
+<script>
+    let {...icon} = $props();
+</script>
+
+<a  class="text-2xl text-(--info)"
+    href={icon.href}     
+    aria-label={icon.label}
+    target={icon.target}
+><i class={icon.name}></i></a>
+
+<style>
+    a:hover { transform: translateY(-2px); }
+</style>
