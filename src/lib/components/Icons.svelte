16ea2ea6b4ae8567334209d55076d417fd7fb82d
Jesus Cordova <jescordova@gmail,com>
2025-09-24 14:20:53 +0200

    added title and color for icon component

--- src/lib/components/Icons.svelte
+++ src/lib/components/Icons.svelte
@@ -2,10 +2,11 @@
     let {...icon} = $props();
 </script>
 
-<a  class="text-2xl text-(--info)"
+<a  class="text-2xl text-(--info) cursor-pointer"
     href={icon.href}     
     aria-label={icon.label}
     target={icon.target}
+    title={icon.label}
 ><i class={icon.name}></i></a>
 
 <style>
