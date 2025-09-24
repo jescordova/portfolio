6c0140b4350c5a3defabcfc82de3e8693a23d323
Jesus Cordova <jescordova@gmail,com>
2025-09-24 04:28:17 +0200

    Added animation for icons

--- src/lib/components/skeleton/Footer.svelte
+++ src/lib/components/skeleton/Footer.svelte
@@ -6,7 +6,7 @@
 
 <footer class="items-center grid md:grid-cols-2 mt-2">
     <p class="text-center md:text-start">&copy; Tous les droits réservés | <a href="/" class="capitalize text-(--info)">yasta web</a> 2025</p>
-    <div class="text-center md:text-end">
+    <div class="flex  justify-center md:justify-end">
         {#each social as icon}
             <Icons {...icon} />
         {/each}
