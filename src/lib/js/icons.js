edebe1c60370634e36bdf7beab8e78bc460a5bc0
Jesus Cordova <jescordova@gmail,com>
2025-09-24 12:29:58 +0200

    Added user contact icons

--- src/lib/js/icons.js
+++ src/lib/js/icons.js
@@ -1,3 +1,36 @@
+export const contacts = [
+    {
+        href: 'mailto:jescordova@gmail.com', 
+        label: 'email', 
+        name: 'bx bxs-envelope', 
+        target: '_parent'
+    },
+    {
+        href: 'tel:+32479069836', 
+        label: 'phone', 
+        name: 'bx bxs-phone', 
+        target: '_parent'
+    },
+    {
+        href: 'https://maps.app.goo.gl/jeptJdup6PZa91uq7', 
+        label: 'map', 
+        name: 'bx bxs-map', 
+        target: '_blanc'
+    },
+    {
+        href: 'https://portfolio-yasta.netlify.app/', 
+        label: 'portfolio', 
+        name: 'bx bxs-folder', 
+        target: '_blanc'
+    },
+    {
+        href: '/cv/', 
+        label: 'curriculum-vitae', 
+        name: 'bx bx-file', 
+        target: '_blanc'
+    },
+]
+
 export const social = [
     {
         href: 'https://portfolio-yasta.netlify.app/', 
