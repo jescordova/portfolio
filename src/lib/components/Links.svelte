b9200021134104d5d6485bc672ed14464c332781
Jesus Cordova <jescordova@gmail,com>
2025-09-25 21:54:11 +0200

    Created Link component

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { project } = $props();
+</script>
+
+<div class="link">
+    <a href={project.href} class="text-base text-(--info)">{project.name} :</a>
+    <span>avec - {project.apps}</span>
+</div>
+<span>[ {project.year} ]</span>
