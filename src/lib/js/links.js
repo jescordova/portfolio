91267f86b13b45b81b9294f600eacbc3b28d2217
Jesus Cordova <jescordova@gmail,com>
2025-09-25 22:01:03 +0200

    Added slideshos link

---
+++ src/lib/js/links.js
@@ -0,0 +1,8 @@
+export const projects = [
+    {
+        href: '/projects/slideshows/',
+        name: 'Slideshows',
+        apps: [ "SvelteKit", " TailwindCss", " JavaScript." ],
+        year: '2023'
+    },
+]
