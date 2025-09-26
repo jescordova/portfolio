6b141e70ffc6a3756daf29a2fc1245bfe291fb98
Jesus Cordova <jescordova@gmail,com>
2025-09-26 10:29:06 +0200

    Added images and caption

---
+++ src/lib/js/slides.js
@@ -0,0 +1,23 @@
+export const galapagos = 
+[
+    {
+        src: 'https://www.voyage-galapagos.com/wp-content/uploads/voyage-iles-galapagos-23-320x180.jpg',
+        caption: 'Île Espagnole',
+    },
+    {
+        src: 'https://www.voyage-galapagos.com/wp-content/uploads/voyage-iles-galapagos-32-1-320x180.jpg',
+        caption: 'Île Bartolome',
+    },
+    {
+        src: 'https://www.voyage-galapagos.com/wp-content/uploads/voyage-galapagos-10-320x180.jpg',
+        caption: 'Île de Wolf',
+    },
+    {
+        src: 'https://www.voyage-galapagos.com/wp-content/uploads/voyage-galapagos-8-320x180.jpg',
+        caption: 'Île de Darwin',
+    },
+    {
+        src: 'https://www.voyage-galapagos.com/wp-content/uploads/voyage-galapagos-1-320x180.jpg',
+        caption: 'Île de Genovesa',
+    },
+]
