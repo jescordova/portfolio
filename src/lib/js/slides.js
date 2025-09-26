2e70abd77c33a9f34c5acafb34f52e225af1ceb3
Jesus Cordova <jescordova@gmail,com>
2025-09-26 15:44:59 +0200

    Added images and caption

--- src/lib/js/slides.js
+++ src/lib/js/slides.js
@@ -21,3 +21,30 @@ export const galapagos =
         caption: 'Île de Genovesa',
     },
 ]
+
+export const belgium = [
+    {
+        src: 'https://img-4.linternaute.com/fFAD8Vbt65UPUccF4P4ER3PPooQ=/1500x/smart/77203446a6b24ebfa7e55b096afd613d/ccmcms-linternaute/12988808.jpg',
+        caption: 'Bruges en ce moment',
+    },
+    {
+        src: 'https://cdn.generationvoyage.fr/2021/06/Dinant.jpg',
+        caption: 'Visiter la Belgique',
+    },
+    {
+        src: 'https://ulysse.com/news/wp-content/uploads/2024/07/Le-quartier-du-Canal-Vert-Bruges-Belgique-.jpg',
+        caption: 'Les quartiers idéaux pour séjourner'
+    },
+    {
+        src: 'https://beauxvillages.be/wp-content/uploads/2021/11/02-COND-SOSOYE-Copyright-C.Collet-1-1024x597.jpg',
+        caption: 'Sosoye',
+    },
+    {
+        src: 'https://beauxvillages.be/wp-content/uploads/2021/11/02-COND-crupet-neige-HQ-9-of-38-Copyright-Bernard-Dacier-2048x1367.jpg',
+        caption: 'Crupet sous la neige',
+    },
+    {
+        src: 'https://beauxvillages.be/wp-content/uploads/2021/01/Willy_PIERRE_Clermont-haut-en-couleur-HDR.jpg',
+        caption: 'Clermont-sur-Berwinne',
+    },
+]
